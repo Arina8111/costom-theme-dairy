f5895bdf94df798e3ff67379b97cb91a1b0c1a2d
Arina8111 <arinababbar@hotmail.com>
2026-07-26 18:46:18 +0530

    Adding files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { Navbar } from './components/nav'
+
+function App() {
+  return (
+    <Navbar/>
+  )
+}
+
+export default App
---
+++ frontend/src/components/nav.jsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+    return (
+        <ul>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+        </ul>
+    )
+}
